ffed3416a9bf3bbd926283dbc2373088e7c57823
Rafael Bueno <rafaelbuenolink@gmail.com>
2022-06-24 15:34:43 -0300

    add initial whatsapp config

--- src/services/handleDealByApp/index.ts
+++ src/services/handleDealByApp/index.ts
@@ -8,6 +8,8 @@ const handleWhatAppMessage = async (app: any, body: any) => {
         console.log('item', item)
         item.changes.forEach((change: any) => {
             console.log('change =>', change)
+            console.log('change.value.contacts', change.value.contacts)
+            console.log('change.value.messages', change.value.messages)
         })
     })
 }
